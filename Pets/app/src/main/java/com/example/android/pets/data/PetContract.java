d900c25f0bcac0d3a368f774afb391b8a262187a
nitin_jain <njain071@gmail.com>
2018-07-03 00:03:42 +0530

    List and ListView functionality

---
+++ Pets/app/src/main/java/com/example/android/pets/data/PetContract.java
@@ -0,0 +1,31 @@
+package com.example.android.pets.data;
+
+import android.provider.BaseColumns;
+
+/**
+ * API Contract for the Pets app.
+ */
+public final class PetContract {
+
+    // To prevent someone from accidentally instantiating the contract class,
+    // give it an empty constructor.
+    private PetContract() {}
+
+
+    public static final class PetEntry implements BaseColumns {
+
+        /** Name of database table for pets */
+        public final static String TABLE_NAME = "pets";
+
+        public final static String _ID = BaseColumns._ID;
+        public final static String COLUMN_PET_NAME ="name";
+        public final static String COLUMN_PET_BREED = "breed";
+        public final static String COLUMN_PET_GENDER = "gender";
+        public final static String COLUMN_PET_WEIGHT = "weight";
+
+        public static final int GENDER_UNKNOWN = 0;
+        public static final int GENDER_MALE = 1;
+        public static final int GENDER_FEMALE = 2;
+    }
+
+}
